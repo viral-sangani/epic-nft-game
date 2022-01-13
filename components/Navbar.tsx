7fcd65e6dd33f0e97bc508424967027c03ec7a78
Viral Sangani <viralsangani@Viral-Sangani-B479s-MacBook-Pro.local>
2022-01-13 13:23:43 +0530

    bug

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -12,12 +12,14 @@ export const Navbar = () => {
     <>
       <nav className="flex items-center w-full h-24 select-none">
         <div className="relative flex flex-wrap items-center justify-between w-full h-24 mx-auto font-medium md:justify-center">
-          <a href="#_" className="w-1/4 py-4 pl-6 pr-4 md:pl-4 md:py-0">
-            <span className="p-1 text-xl font-black leading-none text-white select-none">
-              <span>Epic NFT Game</span>
-              <span className="text-indigo-300">.</span>
-            </span>
-          </a>
+          <Link href="/">
+            <a className="w-1/4 py-4 pl-6 pr-4 md:pl-4 md:py-0">
+              <span className="p-1 text-xl font-black leading-none text-white select-none">
+                <span>Epic NFT Game</span>
+                <span className="text-indigo-300">.</span>
+              </span>
+            </a>
+          </Link>
           <div className="fixed top-0 left-0 z-40 items-center hidden w-full h-full p-3 text-xl bg-gray-900 bg-opacity-50 md:text-sm lg:text-base md:w-3/4 md:bg-transparent md:p-0 md:relative md:flex">
             <div className="flex-col w-full h-full overflow-hidden bg-white rounded-lg select-none md:bg-transparent md:rounded-none md:relative md:flex md:flex-row md:overflow-auto">
               <div className="flex flex-col items-center justify-center w-full h-full mt-12 text-center text-indigo-700 md:text-indigo-200 md:w-2/3 md:mt-0 md:flex-row md:items-center">
